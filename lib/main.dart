e7d73ead69dd16dd0df9ae7c9c80a78509dfe5f6
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-08-29 10:49:29 -0300

    :recycle: refactor add news routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: unused_import
 import 'package:flutter/material.dart';
+import 'package:laborus_app/pages/chat/view/chat.dart';
+
 import 'package:laborus_app/pages/home/home.dart';
 import 'package:laborus_app/pages/loading/load.dart';
+import 'package:laborus_app/pages/notification/model/notification.dart';
+import 'package:laborus_app/pages/notification/view/notification_view.dart';
 import 'package:laborus_app/pages/profile/view/profile.dart';
 import 'package:laborus_app/utils/routes.dart';
 import 'package:laborus_app/utils/theme/dark_theme.dart';
@@ -17,11 +21,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Laborus',
-      theme:  darkTheme,
-      home: const Load(),
+      theme:  lightTheme,
+
+      initialRoute: AppRoutes.start,
       routes: {
+        AppRoutes.start:(ctx) => const Load(),
         AppRoutes.home: (ctx) => const Home(),
         AppRoutes.profile: (ctx) => const Profile(),
+        AppRoutes.notification: (ctx) => const Notifications(),
+        AppRoutes.messages: (ctx) => const Chat(),
       },
       debugShowCheckedModeBanner: false,
     );
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,8 @@
 class AppRoutes{
+  static const start = '/';
   static const home = '/home';
   static const messages = '/messages';
   static const profile = '/profile';
+  static const notification = '/notifications';
+  static const config = '/config';
 }
