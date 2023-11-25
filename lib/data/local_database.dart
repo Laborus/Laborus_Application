8c0203ca2abc1316ee5379cea2c9cef39219ba9a
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-11-25 13:42:10 -0300

    :sparkles: feat(On boarding show) Check where it is open or not using hive for local storage

---
+++ lib/data/local_database.dart
@@ -0,0 +1,31 @@
+import 'package:hive/hive.dart';
+
+class LocalDatabase {
+  static const String boxName = 'themeBox';
+  static const String _onboardingKey = 'onboardingShown';
+
+  static Future<void> saveTheme(bool isDarkMode) async {
+    final box = await Hive.openBox(boxName);
+    await box.put('isDarkMode', isDarkMode);
+    await box.close();
+  }
+
+  static Future<bool> getTheme() async {
+    final box = await Hive.openBox(boxName);
+    final isDarkMode = box.get('isDarkMode', defaultValue: false);
+    await box.close();
+    return isDarkMode;
+  }
+
+  static Future<void> setOnboardingShown() async {
+    final box = await Hive.openBox('app_data');
+    await box.put(_onboardingKey, true);
+    await box.close();
+  }
+
+  static Future<bool> isOnboardingShown() async {
+    final box = await Hive.openBox('app_data');
+    final result = box.get(_onboardingKey, defaultValue: false);
+    return result;
+  }
+}
