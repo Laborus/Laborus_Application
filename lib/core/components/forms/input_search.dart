4740c319983e7355dceff70dcf63ddf1a40f3c18
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2024-08-08 12:17:07 -0300

    style(Input): restyling the search input

--- lib/core/components/forms/input_search.dart
+++ lib/core/components/forms/input_search.dart
@@ -5,30 +5,35 @@ class InputSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.onSecondary,
-        borderRadius: BorderRadius.circular(12),
+    return TextField(
+      cursorColor: Theme.of(context).colorScheme.tertiaryContainer,
+      style: TextStyle(
+        color: Theme.of(context).colorScheme.tertiaryContainer,
       ),
-      child: TextField(
-        cursorColor: Theme.of(context).colorScheme.secondary,
-        style: TextStyle(
-          color: Theme.of(context).colorScheme.secondary,
+      decoration: InputDecoration(
+        fillColor: Theme.of(context).colorScheme.onPrimary,
+        filled: true, // This makes the TextField filled with color
+        suffixIcon: Icon(
+          Icons.search,
+          color: Theme.of(context).colorScheme.tertiaryContainer,
         ),
-        decoration: InputDecoration(
-          suffixIcon: Icon(
-            Icons.search,
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-          hintText: 'Digite alguma coisa...',
-          hintStyle: TextStyle(
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-          disabledBorder: InputBorder.none,
-          border: InputBorder.none,
-          enabledBorder: InputBorder.none,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+        hintText: 'Digite alguma coisa...',
+        hintStyle: TextStyle(
+          color: Theme.of(context).colorScheme.tertiaryContainer,
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(999),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(999),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(999),
+          borderSide: BorderSide.none,
         ),
       ),
     );
