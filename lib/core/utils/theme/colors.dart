4275938a1a663b40328e80fbd49d6f10eb9ae625
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2024-08-13 10:50:59 -0300

    style(color): Updates lightGreen color to FFDDFFEB

--- lib/core/utils/theme/colors.dart
+++ lib/core/utils/theme/colors.dart
@@ -37,7 +37,7 @@ class AppColors {
   static const Color yellow = Color(0xFFFFB800);
   static const Color lightYellow = Color(0xFFFFF9DE);
   static const Color green = Color(0xFF3BC171);
-  static const Color lightGreen = Color(0xFFEBF9F1);
+  static const Color lightGreen = Color(0xFFDDFFEB);
   static const Color violet = Color(0xFF7B61FF);
   static const Color lightViolet = Color(0xFFF2EFFF);
   static const Color lightPurple = Color(0xFFF6EBFF);
